36da8ba9586a700bc3ebe622c5313d00bc32d2ea
Mark Freeman <31807419+mfreema1@users.noreply.github.com>
2017-09-30 16:55:01 -0400

    Moving branch to master
    Copying over patch 9/28/2017 branch to master and closing off that branch.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# udacity-myreads" 
+# MyReads Project Iteration 9/28/2017
+
+Swapped state-hardcoded routing into using React Router DOM in order to give the user the
+use of the back button, as well as to make pages bookmark-able.
+
+Removed anchor tags, replaced with `<Link>` tags.  Checks of state were replaced with `<Route>` tags.  Entire
+app in `index.js` wrapped with `<BrowserRouter>` tag.
---
+++ src/App.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import {Route, Link} from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+
+class BooksApp extends React.Component {
+
+  render() {
+    return (
+        <div className="app">
+          <Route path="/search" render={() => (
+            <div className="search-books">
+            <div className="search-books-bar">
+              <Link className="close-search" to={{pathname: '/'}}>Close</Link>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author"/>
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        )}/> {/*End of first route*/}
+          <Route exact path="/" render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <div className="bookshelf-books">
+                      <ol className="books-grid">
+                        <Book title="To Kill a Mockingbird" authors="Harper Lee" width="128" height="193" url="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"/>
+                        <Book title="Ender's Game" authors="Orson Scott Card" width="128" height="188" url="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"/>
+                      </ol>
+                    </div>
+                  </div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">Want to Read</h2>
+                    <div className="bookshelf-books">
+                      <ol className="books-grid">
+                        <Book title="1776" authors="Davic McCullough" width="128" height="193" url="http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"/>
+                        <Book title="Harry Potter and the Sorcerer's Stone" authors="J.K. Rowling" width="128" height="192" url="http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"/>
+                      </ol>
+                    </div>
+                  </div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title">Read</h2>
+                    <div className="bookshelf-books">
+                      <ol className="books-grid">
+                        <Book title="The Hobbit" authors="J.R.R. Tolkien" width="128" height="192" url="http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"/>
+                        <Book title="Oh, the Places You'll Go!" authors="Seuss" width="128" height="174" url="http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"/>
+                        <Book title="The Adventures of Tam Sawyer" authors="Mark Twain" width="128" height="192" url="http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"/>
+                      </ol>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to={{pathname: '/search'}}>Add a book</Link>
+              </div>
+            </div>
+          )}/>{/*end of second route*/}
+        </div>
+    )
+  }
+}
+
+class Book extends React.Component {
+
+  state = {
+    section: ""
+  }
+
+  render() {
+    return (
+      //@return: a book item to be displayed
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: this.props.width, height: this.props.height, backgroundImage: 'url(' + this.props.url + ')' }}></div>
+            <BookDropdown />
+          </div>
+          <div className="book-title">{this.props.title}</div>
+          <div className="book-authors">{this.props.authors}</div>
+        </div>
+      </li>
+    )
+  }
+}
+
+class BookDropdown extends React.Component {
+  render() {
+    return (
+      //@return: a dropwdown menu that is on each book in the app.  These values
+      //only occur here, so hardcoding them is fine.
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+    )
+  }
+}
+
+class Section extends React.Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <SectionHeader headerText={this.props.headerText}/>
+        <ListOfBooks />
+      </div>
+    )
+  }
+}
+
+//TODO: Finish up this section, it needs some work on the naming of
+//this.props, and needs to match that commented out bit at the bottomff
+
+class SectionHeader extends React.Component {
+  render() {
+    return (
+      <h2 className="bookshelf-title">{this.props.headerText}</h2>
+    )
+  }
+}
+
+class ListOfBooks extends React.Component {
+  render() {
+    return (
+      <div className="bookshelf-books">
+        <ol>
+          <Book />
+          <Book />
+        </ol>
+      </div>
+    )
+  }
+}
+
+{/* <div className="bookshelf">
+  <h2 className="bookshelf-title">Want to Read</h2>
+  <div className="bookshelf-books">
+    <ol className="books-grid">
+      <li>
+        <Book title="1776" authors="Davic McCullough" width="128" height="193" url="http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"/>
+      </li>
+      <li>
+        <Book title="Harry Potter and the Sorcerer's Stone" authors="J.K. Rowling" width="128" height="192" url="http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"/>
+      </li>
+    </ol>
+  </div>
+</div> */}
+
+export default BooksApp
