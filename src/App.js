2a887b318fed3d14bebf560c4672003f9be46218
Mark Freeman <31807419+mfreema1@users.noreply.github.com>
2017-10-01 14:45:11 -0400

    Update App.js
    Transferred more of the standard HTML into react components, so now all of the sections are denoted by the `<Section>` component.  Added a `section` property to each book element.  This will need to be used to set the initial state of the book.  From there, the dropdown will handle all state changes.

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ class BooksApp extends React.Component {
               {/* <SearchReults /> */}
             </div>
           </div>
-        )}/> {/*End of first route*/}
+        )}/> {/*end of first route*/}
           <Route exact path="/" render={() => (
             <div className="list-books">
               <div className="list-books-title">
@@ -70,25 +70,8 @@ class BooksApp extends React.Component {
               <div className="list-books-content">
                 <div>
                   <Section headerText="Currently Reading" sectionName="Currently Reading"/>
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Want to Read</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-                        <Book title="1776" authors="Davic McCullough" url="http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"/>
-                        <Book title="Harry Potter and the Sorcerer's Stone" authors="J.K. Rowling" url="http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"/>
-                      </ol>
-                    </div>
-                  </div>
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Read</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-                        <Book title="The Hobbit" authors="J.R.R. Tolkien" url="http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"/>
-                        <Book title="Oh, the Places You'll Go!" authors="Seuss" url="http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"/>
-                        <Book title="The Adventures of Tam Sawyer" authors="Mark Twain" url="http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"/>
-                      </ol>
-                    </div>
-                  </div>
+                  <Section headerText="Want to Read" sectionName="Want to Read"/>
+                  <Section headerText="Read" sectionName="Read"/>
                 </div>
               </div>
               <div className="open-search">
@@ -114,7 +97,7 @@ class Book extends React.Component {
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + this.props.url + ')' }}></div>
-            <BookDropdown />
+            <BookDropdown selection={this.props.section}/>
           </div>
           <div className="book-title">{this.props.title}</div>
           <div className="book-authors">{this.props.authors}</div>
@@ -125,7 +108,9 @@ class Book extends React.Component {
 }
 
 class BookDropdown extends React.Component {
+
   render() {
+
     return (
       //@return: a dropwdown menu that is on each book in the app.  These values
       //only occur here, so hardcoding them is fine.
@@ -145,6 +130,7 @@ class BookDropdown extends React.Component {
 class Section extends React.Component {
   render() {
     return (
+      //@return: one of the three sections of the MyReads app
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.headerText}</h2>
         <ListOfBooks sectionName={this.props.sectionName}/>
@@ -161,9 +147,10 @@ class ListOfBooks extends React.Component {
 
   render() {
     var booksInSection = this.state.books.map(book =>
-      <Book title={book.title} authors={book.authors} url={book.url} />)
+      <Book title={book.title} authors={book.authors} url={book.url} section={book.section}/>)
 
     return (
+      //@return: set of books filtered for the particular section
       <div className="bookshelf-books">
         <ol className="books-grid">
           {booksInSection}
