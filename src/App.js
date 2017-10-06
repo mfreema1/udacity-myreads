05fc7519907b4b1e8cce497ae12ffa99fe3d8ff6
Mark Freeman <31807419+mfreema1@users.noreply.github.com>
2017-10-06 17:32:18 -0400

    Update App.js
    Removing in-memory starter array and adding in search-functionality.  To get data persistence, the data will need to be offloaded to the server using the provided API.

--- src/App.js
+++ src/App.js
@@ -3,63 +3,25 @@ import {Route, Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-//starting example library
-var startingBookSet = [
-  {
-    title: "1776",
-    authors: "Davic McCullough",
-    section: "Currently Reading",
-    url: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-  },
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    authors: "J.K. Rowling",
-    section: "Currently Reading",
-    url: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-  },
-  {
-    title: "The Hobbit",
-    authors: "J.R.R. Tolkien",
-    section: "Want to Read",
-    url: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
-  },
-  {
-    title: "Oh, the Places You'll Go!",
-    authors: "Seuss",
-    section: "Want to Read",
-    url: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api"
-  },
-  {
-    title: "The Adventures of Tom Sawyer",
-    authors: "Mark Twain",
-    section: "Read",
-    url: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api"
-  },
-  {
-    title: "To Kill a Mockingbird",
-    authors: "Harper Lee",
-    section: "Read",
-    url: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
-  },
-  {
-    title: "Ender's Game",
-    authors: "Orscon Scott Card",
-    section: "Read",
-    url: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-  }
-]
-
 class BooksApp extends React.Component {
 
   state = {
-    booksInLibrary : startingBookSet
+    booksInLibrary : []
   }
 
   //method passed down into the book dropdown.  Updates state of the library according to what was passed back up.
   updateStore = (updatedBook) => {
+    console.log(updatedBook)
     var bookOfInterest = this.state.booksInLibrary.filter(book => book.url === updatedBook.url)[0]
-    var indexOfInterest = this.state.booksInLibrary.indexOf(bookOfInterest)
-    this.state.booksInLibrary[indexOfInterest].section = updatedBook.section
+    //if the book isn't in the library, add it
+    if(!bookOfInterest){
+      this.state.booksInLibrary.push(updatedBook)
+    }
+    //otherwise just update the section of it
+    else{
+      var indexOfInterest = this.state.booksInLibrary.indexOf(bookOfInterest)
+      this.state.booksInLibrary[indexOfInterest].section = updatedBook.section
+    }
     this.setState({booksInLibrary: this.state.booksInLibrary})
   }
 
@@ -72,8 +34,7 @@ class BooksApp extends React.Component {
             <div className="search-books">
             <div className="search-books-bar">
               <Link className="close-search" to={{pathname: '/'}}>Close</Link>
-              <SearchForm />
-              {/* <SearchReults /> */}
+              <SearchForm update={this.updateStore}/>
             </div>
           </div>
         )}/>
@@ -84,9 +45,9 @@ class BooksApp extends React.Component {
               </div>
               <div className="list-books-content">
                 <div>
-                  <Section sectionName="Currently Reading" update={this.updateStore}/>
-                  <Section sectionName="Want to Read" update={this.updateStore}/>
-                  <Section sectionName="Read" update={this.updateStore}/>
+                  <Section sectionName="Currently Reading" update={this.updateStore} bookSet={this.state.booksInLibrary}/>
+                  <Section sectionName="Want to Read" update={this.updateStore} bookSet={this.state.booksInLibrary}/>
+                  <Section sectionName="Read" update={this.updateStore} bookSet={this.state.booksInLibrary}/>
                 </div>
               </div>
               <div className="open-search">
@@ -106,7 +67,7 @@ const Book = (props) => (
     <div className="book">
       <div className="book-top">
         <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + props.url + ')' }}></div>
-        <BookDropdown update={props.update} bookUrl={props.url} section={props.sectionName}/>
+        <BookDropdown update={props.update} bookUrl={props.url} title={props.title} authors={props.authors} section={props.sectionName}/>
       </div>
       <div className="book-title">{props.title}</div>
       <div className="book-authors">{props.authors}</div>
@@ -121,7 +82,9 @@ class BookDropdown extends React.Component {
     var currentBook = document.getElementById(this.props.bookUrl)
     var bookForUpdate = {
       section: currentBook.options[currentBook.selectedIndex].value,
-      url: this.props.bookUrl
+      url: this.props.bookUrl,
+      title: this.props.title,
+      authors: this.props.authors
     }
     this.props.update(bookForUpdate)
   }
@@ -152,7 +115,7 @@ class BookDropdown extends React.Component {
 class Section extends React.Component {
 
   render() {
-    var booksInSection = startingBookSet.filter(book => book.section === this.props.sectionName).map(book =>
+    var booksInSection = this.props.bookSet.filter(book => book.section === this.props.sectionName).map(book =>
       <Book title={book.title} authors={book.authors} url={book.url} update={this.props.update} sectionName={this.props.sectionName}/>)
 
     return (
@@ -171,24 +134,32 @@ class Section extends React.Component {
 }
 
 class SearchForm extends React.Component {
-  state = { query : '' }
+  state = {
+    query : '',
+    booksInForm: []
+  }
   handleSearch = (event) => {
     this.setState({ query : event.target.value })
+    this.searchAPI()
   }
-  render() {
-    return (
-      <div className="search-books-input-wrapper">
-        <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={this.handleSearch}/>
-      </div>
+  searchAPI = () => {
+    BooksAPI.search(this.state.query, 5).then(books =>
+      this.setState({booksInForm: books})
     )
   }
-}
-
-class SearchResults extends React.Component {
   render() {
+    var booksToDisplay = this.state.booksInForm.map(book =>
+      <Book title={book.title} authors={book.authors[0]} url={book.imageLinks.thumbnail} update={this.props.update} sectionName='None'/>)
     return (
-      <div className="search-books-results">
-        <ol className="books-grid"></ol>
+      <div>
+        <div className="search-books-input-wrapper">
+          <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={this.handleSearch}/>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {booksToDisplay}
+          </ol>
+        </div>
       </div>
     )
   }
