b360ec1c406f1a0f16f05aa8cb05c818d39bb491
Mark Freeman <31807419+mfreema1@users.noreply.github.com>
2017-10-12 23:33:29 -0400

    Update App.js
    Incorporated more API calls to allow for data persistence.  The application now strictly utilizes callbacks to access data that is sitting on the server rather than keeping it in local memory.  Also transitioned some additional functions to ES6 arrow functions, reworked some of the properties being passed in the hierarchy (mainly trying to reduce number of props), moved the `Section` field higher up in the code closer to its first implementation, and switched the `values` of the dropdown to reflect the JSON response from the server in order to compare against it in the `filter()` call inside of `Section`.

--- src/App.js
+++ src/App.js
@@ -9,24 +9,19 @@ class BooksApp extends React.Component {
     booksInLibrary : []
   }
 
-  //method passed down into the book dropdown.  Updates state of the library according to what was passed back up.
-  updateStore = (updatedBook) => {
-    console.log(updatedBook)
-    var bookOfInterest = this.state.booksInLibrary.filter(book => book.url === updatedBook.url)[0]
-    //if the book isn't in the library, add it
-    if(!bookOfInterest){
-      this.state.booksInLibrary.push(updatedBook)
-    }
-    //otherwise just update the section of it
-    else{
-      var indexOfInterest = this.state.booksInLibrary.indexOf(bookOfInterest)
-      this.state.booksInLibrary[indexOfInterest].section = updatedBook.section
-    }
-    this.setState({booksInLibrary: this.state.booksInLibrary})
+  viewLibrary = () => {
+    BooksAPI.getAll().then(library => {
+      this.setState({booksInLibrary: library})
+    })
+  }
+
+  updateLibrary = (bookId, shelf) => {
+    BooksAPI.update(bookId, shelf).then(this.viewLibrary())
+    //this returns an array of strings. these need to be mapped back to their book objects
   }
 
   render() {
-
+    this.viewLibrary()
     //@returns the entire application
     return (
         <div className="app">
@@ -34,7 +29,7 @@ class BooksApp extends React.Component {
             <div className="search-books">
             <div className="search-books-bar">
               <Link className="close-search" to={{pathname: '/'}}>Close</Link>
-              <SearchForm update={this.updateStore}/>
+              <SearchForm update={this.updateLibrary}/>
             </div>
           </div>
         )}/>
@@ -45,9 +40,9 @@ class BooksApp extends React.Component {
               </div>
               <div className="list-books-content">
                 <div>
-                  <Section sectionName="Currently Reading" update={this.updateStore} bookSet={this.state.booksInLibrary}/>
-                  <Section sectionName="Want to Read" update={this.updateStore} bookSet={this.state.booksInLibrary}/>
-                  <Section sectionName="Read" update={this.updateStore} bookSet={this.state.booksInLibrary}/>
+                  <Section text="Currently Reading" name="currentlyReading" update={this.updateLibrary} bookSet={this.state.booksInLibrary}/>
+                  <Section text="Want to Read" name="wantToRead" update={this.updateLibrary} bookSet={this.state.booksInLibrary}/>
+                  <Section text="Read" name="read" update={this.updateLibrary} bookSet={this.state.booksInLibrary}/>
                 </div>
               </div>
               <div className="open-search">
@@ -60,14 +55,35 @@ class BooksApp extends React.Component {
   }
 }
 
+class Section extends React.Component {
+
+  render() {
+    var booksInSection = this.props.bookSet.filter(book => book.shelf === this.props.name).map(book =>
+      <Book title={book.title} authors={book.authors} imageUrl={book.imageLinks.thumbnail} bookId={book.id} update={this.props.update} shelf={this.props.text}/>)
+
+    return (
+      //@return: one of the three sections of the app.  Each section gets a filtered
+      //copy of the main
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.text}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {booksInSection}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
 const Book = (props) => (
   //@return a book in the application.  Only has render() function, so was
   //reduced to a functional component
   <li>
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + props.url + ')' }}></div>
-        <BookDropdown update={props.update} bookUrl={props.url} title={props.title} authors={props.authors} section={props.sectionName}/>
+        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + props.imageUrl + ')' }}></div>
+        <BookDropdown update={props.update} bookId={props.bookId} shelf={props.shelf}/>
       </div>
       <div className="book-title">{props.title}</div>
       <div className="book-authors">{props.authors}</div>
@@ -76,59 +92,30 @@ const Book = (props) => (
 )
 
 class BookDropdown extends React.Component {
-
   //passes data up to the application for state changes
   handleUpdate = (select) => {
-    var currentBook = document.getElementById(this.props.bookUrl)
-    var bookForUpdate = {
-      section: currentBook.options[currentBook.selectedIndex].value,
-      url: this.props.bookUrl,
-      title: this.props.title,
-      authors: this.props.authors
-    }
-    this.props.update(bookForUpdate)
+    var currentBook = document.getElementById(this.props.bookId)
+    this.props.update(currentBook, currentBook.options[currentBook.selectedIndex].value)
   }
-
   //sets the selector to the correct entry for that section
-  componentDidMount() {
-    var selector = document.getElementById(this.props.bookUrl)
-    selector.value = this.props.sectionName
+  componentDidMount = () => {
+    var selector = document.getElementById(this.props.bookId)
+    selector.value = this.props.shelf
   }
 
   render() {
     return (
-      //@return: a dropwdown menu that is on each book in the app.  id is
-      //represented by url as a quick way to get unique id's.
+      //@return: a dropwdown menu that is on each book in the app. Values
+      //were adjusted to fit what the api returns
         <div className="book-shelf-changer">
-          <select id={this.props.bookUrl} onChange={this.handleUpdate.bind(this)}>
-            <option value="None" disabled>Move to...</option>
-            <option value="Currently Reading">Currently Reading</option>
-            <option value="Want to Read">Want to Read</option>
-            <option value="Read">Read</option>
-            <option value="None">None</option>
+          <select id={this.props.bookId} onChange={this.handleUpdate.bind(this)}>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
           </select>
         </div>
-    )
-  }
-}
-
-class Section extends React.Component {
-
-  render() {
-    var booksInSection = this.props.bookSet.filter(book => book.section === this.props.sectionName).map(book =>
-      <Book title={book.title} authors={book.authors} url={book.url} update={this.props.update} sectionName={this.props.sectionName}/>)
-
-    return (
-      //@return: one of the three sections of the app.  Each section gets a filtered
-      //copy of the main
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.sectionName}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {booksInSection}
-          </ol>
-        </div>
-      </div>
     )
   }
 }
@@ -143,13 +130,17 @@ class SearchForm extends React.Component {
     this.searchAPI()
   }
   searchAPI = () => {
+    //@argument I only want 5 results max, but this doesn't seem to work
+    //with the given API documentation
     BooksAPI.search(this.state.query, 5).then(books =>
       this.setState({booksInForm: books})
     )
   }
   render() {
+    //@return: a list of books inside of the form.  This will update as the user
+    //types into the search field
     var booksToDisplay = this.state.booksInForm.map(book =>
-      <Book title={book.title} authors={book.authors[0]} url={book.imageLinks.thumbnail} update={this.props.update} sectionName='None'/>)
+      <Book title={book.title} authors={book.authors} bookId={book.id} imageUrl={book.imageLinks.thumbnail} update={this.props.update} sectionName='None'/>)
     return (
       <div>
         <div className="search-books-input-wrapper">
